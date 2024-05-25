e5d39218f73a5422f12aa890ae1aaeed24247adf
Przemek Kanach <kanach16@gmail.com>
2024-05-25 17:06:48 +0200

    Add proxy

---
+++ proxy/nginx-template-subst.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DNS_SERVER=$( \
+    grep -i '^nameserver' /etc/resolv.conf | head -n1 | cut -d ' ' -f2 \
+)
+export DNS_SERVER
+export APP_UPSTREAM="${APP_UPSTREAM:-"http://app/"}"
+export AUTH_UPSTREAM="${AUTH_UPSTREAM:-"http://auth/"}"
+
+envsubst '$DNS_SERVER $APP_UPSTREAM $AUTH_UPSTREAM' \
+    < /var/nginx.conf > /etc/nginx/conf.d/default.conf
