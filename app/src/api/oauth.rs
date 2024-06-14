877f2f1f56e32575a6e627f6f82bc418d5586318
Przemek Kanach <kanach16@gmail.com>
2024-06-14 20:44:46 +0200

    Refresh token service

--- app/src/api/oauth.rs
+++ app/src/api/oauth.rs
@@ -9,7 +9,7 @@ use axum_extra::extract::cookie::{Cookie, CookieJar, SameSite};
 use serde::Deserialize;
 use time::Duration;
 
-use crate::services::auth::{exchange_code_for_token, TokenExchangeError, TokenParams};
+use crate::services::auth::{obtain_tokens, ObtainTokensError, ObtainTokensParams};
 use crate::state::AppState;
 
 const TOKEN_COOKIES_MAX_AGE_DAYS: i64 = 7;
@@ -28,20 +28,20 @@ async fn callback(
     State(state): State<AppState>,
     jar: CookieJar,
 ) -> impl IntoResponse {
-    let token_params = TokenParams {
+    let params = ObtainTokensParams {
         code: &auth_response.code,
         client_id: &state.config.auth_client_id,
         client_secret: &state.config.auth_client_secret,
         redirect_uri: &state.config.auth_redirect_uri,
     };
-    let token_response = exchange_code_for_token(
+    let tokens = obtain_tokens(
         &state.config.auth_service_internal_url,
         &state.config.auth_realm,
-        token_params,
+        params,
         &state.http_client,
     )
     .await;
-    match token_response {
+    match tokens {
         Ok(value) => {
             let access_token_cookie = build_token_cookie("access_token", value.access_token);
             let refresh_token_cookie = build_token_cookie("refresh_token", value.refresh_token);
@@ -52,17 +52,13 @@ async fn callback(
                 .into_response();
         }
         Err(err) => match err {
-            TokenExchangeError::InvalidRequestError(_) => {
-                tracing::info!("Invalid request when exchanging code for token: {}", err);
-                return (StatusCode::BAD_REQUEST, "Couldn't exchange code for token")
-                    .into_response();
+            ObtainTokensError::InvalidRequestError(_) => {
+                tracing::info!("Invalid request when obtaining tokens: {}", err);
+                return (StatusCode::BAD_REQUEST, "Couldn't obtain tokens").into_response();
             }
             _ => {
-                tracing::error!("Error when exchanging code for token: {}", err);
-                return (
-                    StatusCode::INTERNAL_SERVER_ERROR,
-                    "Couldn't exchange code for token",
-                )
+                tracing::error!("Error when obtaining tokens: {}", err);
+                return (StatusCode::INTERNAL_SERVER_ERROR, "Couldn't obtain tokens")
                     .into_response();
             }
         },
--- app/src/services/auth.rs
+++ app/src/services/auth.rs
@@ -6,7 +6,7 @@ pub fn get_auth_url(base_url: &str, realm: &str, client_id: &str, redirect_uri: 
     format!("{base_url}/realms/{realm}/protocol/openid-connect/auth?response_type=code&scope=openid&client_id={client_id}&redirect_uri={redirect_uri}", base_url=base_url, realm=realm, client_id=client_id, redirect_uri=redirect_uri)
 }
 
-pub struct TokenParams<'a> {
+pub struct ObtainTokensParams<'a> {
     pub code: &'a str,
     pub client_id: &'a str,
     pub client_secret: &'a str,
@@ -14,65 +14,65 @@ pub struct TokenParams<'a> {
 }
 
 #[derive(Deserialize)]
-pub struct TokenResponse {
+pub struct TokensResponse {
     pub access_token: String,
     pub refresh_token: String,
 }
 
-pub enum TokenExchangeError {
+pub enum ObtainTokensError {
     SendingRequestError(Error),
     InvalidRequestError(Error),
     InvalidResponseError(Error),
 }
 
-impl fmt::Display for TokenExchangeError {
+impl fmt::Display for ObtainTokensError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            TokenExchangeError::SendingRequestError(err)
-            | TokenExchangeError::InvalidRequestError(err)
-            | TokenExchangeError::InvalidResponseError(err) => write!(f, "{}", err),
+            ObtainTokensError::SendingRequestError(err)
+            | ObtainTokensError::InvalidRequestError(err)
+            | ObtainTokensError::InvalidResponseError(err) => write!(f, "{}", err),
         }
     }
 }
 
-pub async fn exchange_code_for_token(
+pub async fn obtain_tokens(
     base_url: &str,
     realm: &str,
-    token_params: TokenParams<'_>,
+    params: ObtainTokensParams<'_>,
     http_client: &Client,
-) -> Result<TokenResponse, TokenExchangeError> {
-    let token_url = format!(
+) -> Result<TokensResponse, ObtainTokensError> {
+    let url = format!(
         "{base_url}/realms/{realm}/protocol/openid-connect/token",
         base_url = base_url,
         realm = realm
     );
     let params = [
         ("grant_type", "authorization_code"),
-        ("code", token_params.code),
-        ("client_id", token_params.client_id),
-        ("client_secret", token_params.client_secret),
-        ("redirect_uri", token_params.redirect_uri),
+        ("code", params.code),
+        ("client_id", params.client_id),
+        ("client_secret", params.client_secret),
+        ("redirect_uri", params.redirect_uri),
     ];
 
     let res = http_client
-        .post(&token_url)
+        .post(&url)
         .form(&params)
         .send()
         .await
-        .map_err(TokenExchangeError::SendingRequestError)?;
+        .map_err(ObtainTokensError::SendingRequestError)?;
 
     if res.status().is_client_error() {
-        return Err(TokenExchangeError::InvalidRequestError(
+        return Err(ObtainTokensError::InvalidRequestError(
             res.error_for_status().err().unwrap(),
         ));
     }
     res.error_for_status_ref()
-        .map_err(TokenExchangeError::InvalidResponseError)?;
-    let token_response = res
-        .json::<TokenResponse>()
+        .map_err(ObtainTokensError::InvalidResponseError)?;
+    let tokens = res
+        .json::<TokensResponse>()
         .await
-        .map_err(TokenExchangeError::InvalidResponseError)?;
-    return Ok(token_response);
+        .map_err(ObtainTokensError::InvalidResponseError)?;
+    return Ok(tokens);
 }
 
 #[derive(Deserialize)]
@@ -112,14 +112,14 @@ pub async fn get_jwk(
     kid: &str,
     http_client: &Client,
 ) -> Result<JWKResponse, RetrievingJWKError> {
-    let certificate_url = format!(
+    let url = format!(
         "{base_url}/realms/{realm}/protocol/openid-connect/certs",
         base_url = base_url,
         realm = realm
     );
 
     let res = http_client
-        .get(&certificate_url)
+        .get(&url)
         .send()
         .await
         .map_err(RetrievingJWKError::SendingRequestError)?
@@ -136,4 +136,65 @@ pub async fn get_jwk(
         .find(|key| key.kid == kid)
         .ok_or_else(|| RetrievingJWKError::JWKNotFoundError(kid.to_string()));
     return jwk;
+}
+
+pub struct RefreshTokenParams<'a> {
+    pub refresh_token: &'a str,
+    pub client_id: &'a str,
+    pub client_secret: &'a str,
+}
+
+pub enum RefreshTokenError {
+    SendingRequestError(Error),
+    InvalidRequestError(Error),
+    InvalidResponseError(Error),
+}
+
+impl fmt::Display for RefreshTokenError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            RefreshTokenError::SendingRequestError(err)
+            | RefreshTokenError::InvalidRequestError(err)
+            | RefreshTokenError::InvalidResponseError(err) => write!(f, "{}", err),
+        }
+    }
+}
+
+pub async fn refresh_token(
+    base_url: &str,
+    realm: &str,
+    params: RefreshTokenParams<'_>,
+    http_client: &Client,
+) -> Result<TokensResponse, RefreshTokenError> {
+    let url = format!(
+        "{base_url}/realms/{realm}/protocol/openid-connect/token",
+        base_url = base_url,
+        realm = realm
+    );
+    let params = [
+        ("grant_type", "refresh_token"),
+        ("refresh_token", params.refresh_token),
+        ("client_id", params.client_id),
+        ("client_secret", params.client_secret),
+    ];
+
+    let res = http_client
+        .post(&url)
+        .form(&params)
+        .send()
+        .await
+        .map_err(RefreshTokenError::SendingRequestError)?;
+
+    if res.status().is_client_error() {
+        return Err(RefreshTokenError::InvalidRequestError(
+            res.error_for_status().err().unwrap(),
+        ));
+    }
+    res.error_for_status_ref()
+        .map_err(RefreshTokenError::InvalidResponseError)?;
+    let tokens = res
+        .json::<TokensResponse>()
+        .await
+        .map_err(RefreshTokenError::InvalidResponseError)?;
+    return Ok(tokens);
 }
